d95cce68b7322b24395f5e1a5bb359a85156230b
GrigLog <griglogvin@gmail.com>
2020-10-14 23:04:00 +0300

    And again (last one, I promise)

---
+++ src/main/java/griglog/thaumtweaks/mixins/entities/GolemMixin.java
@@ -0,0 +1,19 @@
+package griglog.thaumtweaks.mixins.entities;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import thaumcraft.api.items.ItemsTC;
+import thaumcraft.common.entities.construct.EntityOwnedConstruct;
+import thaumcraft.common.golems.EntityThaumcraftGolem;
+
+@Mixin(EntityThaumcraftGolem.class)
+public abstract class GolemMixin extends EntityOwnedConstruct {
+    public GolemMixin(World worldIn) {
+        super(worldIn);
+    }
+
+    protected void dropFewItems(boolean flag, int looting) {
+        entityDropItem(new ItemStack(ItemsTC.curio, world.rand.nextInt(3), 2), 0.5F);
+    }
+}
