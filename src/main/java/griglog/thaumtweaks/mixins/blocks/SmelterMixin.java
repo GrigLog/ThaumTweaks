d7dfcdcfb8ea4a2af823c1b2635b739bcd1a5c58
GrigLog <griglogvin@gmail.com>
2020-11-24 10:25:17 +0300

    Essentia Smelters can get fuel from hoppers

---
+++ src/main/java/griglog/thaumtweaks/mixins/blocks/SmelterMixin.java
@@ -0,0 +1,21 @@
+package griglog.thaumtweaks.mixins.blocks;
+
+import net.minecraft.util.EnumFacing;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import thaumcraft.common.tiles.TileThaumcraftInventory;
+import thaumcraft.common.tiles.essentia.TileSmelter;
+
+@Mixin(TileSmelter.class)
+public abstract class SmelterMixin extends TileThaumcraftInventory {
+    private static final int[] slotFuel = new int[]{1};
+    private static final int[] slotItems = new int[]{0};
+
+    public int[] getSlotsForFace(EnumFacing face) {
+        return (face == EnumFacing.DOWN || face == getFacing().getOpposite()) ? slotFuel : slotItems;
+    }
+
+    public SmelterMixin(int size) {
+        super(size);
+    }
+}
