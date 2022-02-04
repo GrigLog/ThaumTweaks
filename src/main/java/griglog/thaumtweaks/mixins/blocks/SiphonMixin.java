5f4659e57d6d784d420cc1bc6c69252c236f2939
GrigLog <griglogvin@gmail.com>
2022-02-04 12:16:32 +0300

    Degrade Void Siphon Mixin to fix LagGoggles incompability #23

--- src/main/java/griglog/thaumtweaks/mixins/blocks/SiphonMixin.java
+++ src/main/java/griglog/thaumtweaks/mixins/blocks/SiphonMixin.java
@@ -1,35 +1,45 @@
 package griglog.thaumtweaks.mixins.blocks;
 
 import griglog.thaumtweaks.TTConfig;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import thaumcraft.api.items.ItemsTC;
 import thaumcraft.common.entities.EntityFluxRift;
 import thaumcraft.common.lib.utils.BlockStateUtils;
+import thaumcraft.common.tiles.TileThaumcraft;
 import thaumcraft.common.tiles.TileThaumcraftInventory;
 import thaumcraft.common.tiles.crafting.TileVoidSiphon;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 @Mixin(TileVoidSiphon.class)
-public abstract class SiphonMixin extends TileThaumcraftInventory {
-    public SiphonMixin() {
-        super(1);
-    }
+public abstract class SiphonMixin extends TileThaumcraft {
 
     public void update() {
-        super.update();
+        if (this.initial) {
+            this.initial = false;
+            if (!this.world.isRemote) {
+                this.syncSlots((EntityPlayerMP)null);
+            } else {
+                NBTTagCompound nbt = new NBTTagCompound();
+                nbt.setBoolean("requestSync", true);
+                this.sendMessageToServer(nbt);
+            }
+        }
         ++this.counter;
         if (!this.getWorld().isRemote && BlockStateUtils.isEnabled(this.getBlockMetadata()) &&
                 this.counter % 20 == 0 && this.progress < cap && notFull()) {
-            List<EntityFluxRift> frl = this.getValidRifts();
+            List<EntityFluxRift> rifts = this.getValidRifts();
             boolean b = false;
 
             double speed;
-            for(Iterator var3 = frl.iterator(); var3.hasNext(); b = speed >= 1.0D) {
-                EntityFluxRift fr = (EntityFluxRift)var3.next();
+            for(Iterator<EntityFluxRift> i = rifts.iterator(); i.hasNext(); b = speed >= 1.0D) {
+                EntityFluxRift fr = i.next();
                 speed = Math.sqrt(fr.getRiftSize());
                 this.progress += speed;
                 fr.setRiftStability((float)(fr.getRiftStability() - speed / 15.0D));
@@ -57,24 +67,29 @@ public abstract class SiphonMixin extends TileThaumcraftInventory {
         }
     }
 
-    void tryAddItem(ItemStack is) {
+    private void tryAddItem(ItemStack is) {
         if (getStackInSlot(0).isEmpty())
             setInventorySlotContents(0, is);
         else if (getStackInSlot(0).getItem() == is.getItem())
             getStackInSlot(0).setCount(getStackInSlot(0).getCount() + 1);
     }
 
-    boolean notFull() {
+    private boolean notFull() {
         return this.getStackInSlot(0).isEmpty() || this.getStackInSlot(0).getItem() == ItemsTC.voidSeed && this.getStackInSlot(0).getCount() < this.getStackInSlot(0).getMaxStackSize();
     }
 
-    @Shadow
-    protected abstract List<EntityFluxRift> getValidRifts();
 
+    @Shadow
+    private List<EntityFluxRift> getValidRifts(){return new ArrayList<>();}
     @Shadow
     int counter;
     @Shadow
     public int progress;
+
+    boolean initial;
+    public abstract ItemStack getStackInSlot(int index);
+    public abstract void setInventorySlotContents(int a, ItemStack b);
+    protected abstract void syncSlots(EntityPlayerMP player);
 
     private static final int cap = (int) (2000 / (TTConfig.voidSiphon.allow ? TTConfig.voidSiphon.speed : 1));
     private static final double reduceChance = 0.03 / (TTConfig.voidSiphon.allow ? TTConfig.voidSiphon.endurance : 1);
